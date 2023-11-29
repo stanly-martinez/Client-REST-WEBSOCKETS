6b112af1eae14a9d9e7d48e84b769fb6cb563729
pizzaroni <stanlymt3zx@gmail.com>
2023-11-28 19:04:45 -0600

    Primer commit con mi proyecto

---
+++ app/assets/javascript/welcome.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('La página de bienvenida se ha cargado completamente.');
+
+  // Obtén los datos directamente desde el atributo data-table-data
+  var jsonData = document.getElementById('tabla-container').dataset.tableData;
+
+  // Intenta analizar la cadena JSON
+  try {
+    var parsedData = JSON.parse(jsonData);
+
+    // Verifica que se haya analizado correctamente
+    if (Array.isArray(parsedData)) {
+      // Crea la tabla y agrega el encabezado
+      var table = document.createElement('table');
+      table.border = '1';
+      var headerRow = table.insertRow();
+
+      ['ID', 'DESDE', 'HASTA', 'ESPACIO'].forEach(function(header) {
+        var th = document.createElement('th');
+        th.textContent = header;
+        headerRow.appendChild(th);
+      });
+
+      // Itera sobre los datos JSON y crea filas
+      parsedData.forEach(function(item) {
+        var row = table.insertRow();
+        ['ID', 'DESDE', 'HASTA', 'ESPACIO'].forEach(function(key) {
+          var cell = row.insertCell();
+          cell.textContent = item[key];
+        });
+      });
+
+      // Agrega la tabla al cuerpo del documento
+      document.body.appendChild(table);
+    } else {
+      console.error('Error: El JSON no es un array.');
+    }
+  } catch (error) {
+    console.error('Error al analizar el JSON:', error);
+  }
+});
+
+// hay error en la linea 9, al parecer parse esta generando conflictos o no esta funcionando, 
+//PERO ya se obtubieron avances y se puede obtener el json del controlador a la vista a js
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,14 @@
+# app/controllers/welcome_controller.rb
+
+class WelcomeController < ApplicationController
+  def index
+    # Aquí se debe obtener el JSON desde REST
+    # JSON de muestra para el diseño
+
+    json_data = '[{"ID": 1, "DESDE": "2023-01-01", "HASTA": "2023-01-10", "ESPACIO": "Oficina A"},
+    {"ID": 2, "DESDE": "2023-01-15", "HASTA": "2023-01-20", "ESPACIO": "Oficina B"}]'
+    
+    @table_data = JSON.parse(json_data)
+    puts "@table_data: #{@table_data.inspect}"
+  end
+end
